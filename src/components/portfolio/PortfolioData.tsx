18a5101745d3f2d6308d068ef2ed7798a3bc9d8f
Space Pocket <space1985pocket@gmail.com>
2024-12-09 17:04:26 +0300

    refactor: add price difference to portfolio

--- src/components/portfolio/PortfolioData.tsx
+++ src/components/portfolio/PortfolioData.tsx
@@ -19,6 +19,7 @@ export const PortfolioData: React.FC<{ handleClose?: () => void }> = () => {
     'Name',
     'Price USD',
     'Price change',
+    'Price difference',
     'Count',
     'Total',
     'Delete',
@@ -27,36 +28,48 @@ export const PortfolioData: React.FC<{ handleClose?: () => void }> = () => {
   const handleDeleteCurrency = (id: string): void => {
     dispatch(portfolioDeleteCurrency(id));
   };
+
   return (
     <>
       {portfolioCurrencies.length > 0 ? (
         <TableData tableCellHeaders={tableCellHeaders}>
-          {portfolioCurrencies.map((currency) => (
-            <TableRow key={currency.id}>
-              <TableCell>{currency.name}</TableCell>
-              <TableCell>{currency.priceUsd}</TableCell>
-              <TableCell align="center">
-                {currency.isChange && <CheckCircleIcon color="primary" />}
-              </TableCell>
-              <TableCell>{currency.count}</TableCell>
-              <TableCell>{currency.total}</TableCell>
-              <TableCell
-                align="center"
-                sx={{ cursor: 'pointer' }}
-                onClick={() => handleDeleteCurrency(currency.id)}
-              >
-                <DeleteIcon />
-              </TableCell>
-            </TableRow>
-          ))}
+          {portfolioCurrencies.map((currency) => {
+            const priceDifference = Number(
+              (Number(currency.priceUsd) - currency.initialPrice).toFixed(3)
+            );
+            return (
+              <TableRow key={currency.id}>
+                <TableCell>{currency.name}</TableCell>
+                <TableCell>{currency.priceUsd}</TableCell>
+                <TableCell align="center">
+                  {currency.isChange && <CheckCircleIcon color="primary" />}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{ color: priceDifference > 0 ? 'green' : 'red' }}
+                >
+                  {currency.isChange && priceDifference}
+                </TableCell>
+                <TableCell>{currency.count}</TableCell>
+                <TableCell>{currency.total}</TableCell>
+                <TableCell
+                  align="center"
+                  sx={{ cursor: 'pointer' }}
+                  onClick={() => handleDeleteCurrency(currency.id)}
+                >
+                  <DeleteIcon />
+                </TableCell>
+              </TableRow>
+            );
+          })}
           <TableRow sx={{ bgcolor: '#ddd7d7' }}>
-            <TableCell colSpan={4}>{'Total cost'}</TableCell>
+            <TableCell colSpan={5}>{'Total cost'}</TableCell>
             <TableCell sx={{ fontWeight: 600 }}>{portfolioTotal}</TableCell>
             <TableCell></TableCell>
           </TableRow>
         </TableData>
       ) : (
-        <SubTitle titleText="Your portfolio is empty! Time to buy something)"></SubTitle>
+        <SubTitle titleText="Your portfolio is empty! Time to buy something!" />
       )}
     </>
   );
